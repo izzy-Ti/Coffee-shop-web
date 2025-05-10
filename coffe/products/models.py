675ffc6e691a9b65b426ddf32bf959e7f796d163
izzy-Ti <israelashenafi29@gmail.com>
2025-05-10 06:50:37 -0700

    edited front

--- coffe/products/models.py
+++ coffe/products/models.py
@@ -6,8 +6,7 @@ class products(models.Model):
     name = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     price = models.IntegerField(max_length=4)
-class MyModel(BaseModel):
-    description = models.TextField()
+
 
     def __str__(self):
         return self.name
