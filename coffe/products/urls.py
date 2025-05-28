7120d6cf793140e88c95931d661802dc97ab0483
izzy-Ti <israelashenafi29@gmail.com>
2025-05-28 12:06:53 -0700

    order accepting page finished

--- coffe/products/urls.py
+++ coffe/products/urls.py
@@ -7,5 +7,7 @@ app_name = 'orders'
 
 urlpatterns = [
         path('order/<int:product_id>/', views.update_orders, name='update_orders'),
-        path('order_list/',views.order_list, name = 'order_list')
+        path('order_list/',views.order_list, name = 'order_list'),
+        path('keeper/', views.login_user , name = "login"),
+        path('successfull/', views.succ, name = 'successfull')
 ]
--- coffe/products/views.py
+++ coffe/products/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render, redirect,get_object_or_404
 from products.models import products, orders
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
-
+def succ(request):
+    return render(request, 'products/success.html')
 def update_orders(request, product_id):
     if request.method == 'POST':
         print("Form submitted!")
@@ -25,7 +30,25 @@ def update_orders(request, product_id):
         )
         order.save()
 
-        return redirect('home')
+        return redirect('orders:successfull')
+@login_required(login_url='orders:login')
 def order_list(request):
     lists = orders.objects.all()
     return render(request, 'products/delivery.html', {'lists': lists})
+
+
+
+def login_user(request):
+    user_aut = False
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate (request=request, username = username, password=password)
+        if user is not None:
+            login(request, user)
+            user_aut = True
+            return redirect('orders:order_list')
+        else:
+            messages.error(request, '*Invalid username or password')
+            return redirect('orders:login')
+    return render(request, 'products/login.html')
